754a6364c9697481da2f4a932f561a4cf63e132f
Madison Hanberry <mhanberry1@gmail.com>
2024-11-11 23:05:49 -0800

    add comments

--- frontend/js/modules/matrix-lib.js
+++ frontend/js/modules/matrix-lib.js
@@ -1,10 +1,6 @@
-export const identity = [
-	[1, 0, 0, 0],
-	[0, 1, 0, 0],
-	[0, 0, 1, 0],
-	[0, 0, 0, 1],
-]
-
+// Multiply two matrices together using dot product multiplication
+//
+// a and b: matrices (2d array of numbers)
 export const matrixMult = (a, b) => {
 	if (a[0]?.length != b.length) {
 		throw "incompatible dimensions"
@@ -28,6 +24,21 @@ export const matrixMult = (a, b) => {
 	return result
 }
 
+// An identity matrix.
+// This won't transform anything, so it's a good starting point
+export const identity = [
+	[1, 0, 0, 0],
+	[0, 1, 0, 0],
+	[0, 0, 1, 0],
+	[0, 0, 0, 1],
+]
+
+// Create a matrix that will translate a
+// coordinate in the x, y, and z dimensions
+//
+// x, y, and z: The amount to translate by (number)
+//
+// returns a matrix (2d array of numbers)
 export const translate = (x, y, z) => [
 	[1, 0, 0, 0],
 	[0, 1, 0, 0],
@@ -35,6 +46,12 @@ export const translate = (x, y, z) => [
 	[x, y, z, 1],
 ]
 
+// Create a matrix that will scale a coordinate
+// in the x, y, and z dimensions
+//
+// x, y, and z: the amount to scale by (number)
+//
+// returns a matrix (2d array of numbers)
 export const scale = (x, y = x, z = x) => [
 	[x, 0, 0, 0],
 	[0, y, 0, 0],
@@ -42,6 +59,12 @@ export const scale = (x, y = x, z = x) => [
 	[0, 0, 0, 1],
 ]
 
+// Create a matrix that will rotate a coordinate
+// along the x, y, and z axes
+//
+// x, y, and z: the amount to rotate by (number)
+//
+// returns a matrix (2d array of numbers)
 export const rotate = (x, y, z) => {
 	// X rotation
 
@@ -82,6 +105,13 @@ export const rotate = (x, y, z) => {
 	return matrixMult(matrixMult(xMat, yMat), zMat)
 }
 
+// Creates a matrix that can project a matrix
+// in 3d space to create a sense of perspective
+//
+// scale: Intensity of the effect
+// aspect: Ratio of the width and height for the viewport
+//
+// returns a matrix (2d array of numbers)
 export const project = (scale, aspect) => [
 	[aspect > 1 ? 1 / aspect : 1, 0, 0, 0],
 	[0, aspect < 1 ? aspect : 1, 0, 0],
