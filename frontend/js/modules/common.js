9af01e8532402ceb5494514b2b22d7848f0061a3
Madison Hanberry <mhanberry1@gmail.com>
2024-11-11 22:42:26 -0800

    added comments

--- frontend/js/modules/common.js
+++ frontend/js/modules/common.js
@@ -1,2 +1,5 @@
+// Return the first element that matches a query selector
 export const $ = document.querySelector.bind(document)
+
+// Return all elements that match a query selector
 export const $$ = document.querySelectorAll.bind(document)
